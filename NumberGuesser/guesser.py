af6141d1c2dc41797af1fbf48f0e1e01427bd1ea
Siraj Ahmadzai <siraj_ahmadzai@hotmail.com>
2018-03-06 15:30:35 -0500

    added guesser program

---
+++ NumberGuesser/guesser.py
@@ -0,0 +1,33 @@
+import random
+
+
+
+guesses_made = 0
+
+
+
+name = input('Hello! What is your name?\n')
+
+number = random.randint(1,20)
+
+print('Well, {0}, I am thinking of a number between 1 and 20 .'.format(name))
+
+while guesses_made < 6:
+    guess = int(input('Take a guess: '))
+
+    guesses_made += 1
+
+    if guess < number:
+        print('your guess is too low.')
+
+    if guess > number:
+            print('your guess is too high.')
+
+    if guess == number:
+                break
+
+
+if guess == number:
+    print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
+else:
+    print('Nope the number I was thinking of was {0}'.format(number))
